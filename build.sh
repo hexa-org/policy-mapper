5bb0ebac851d9c7698b3c4f84c32646eb50353ad
Phil Hunt <phil.hunt@independentid.com>
2023-09-07 10:27:11 -0700

    New build file does multi-module build and test
    Signed-off-by: Phil Hunt <phil.hunt@independentid.com>

--- build.sh
+++ build.sh
@@ -1,27 +1,52 @@
 echo "Building 5 sub-modules in workspace..."
 
+function doTest () {
+  if [ "$test" == "Y" ]
+  then
+    echo "   running tests..."
+    go test ./...
+  fi
+}
+
+test="N"
+while getopts "t" OPTION; do
+  case "$OPTION" in
+    t)
+      echo "Build and test requested."
+      test="Y"
+      ;;
+  esac
+done
+
 go work sync
 
 echo "  hexaIdql."
 
 cd ./hexaIdql
 go build ./...
+doTest
 cd ..
 
 echo "  mapper/conditionLangs/gcpcel."
 cd mapper/conditionLangs/gcpcel
 go build ./...
+doTest
 cd ../../..
 
 echo "  mapper/formats/awsCedar."
 cd mapper/formats/awsCedar
 go build ./...
+doTest
 cd ../../..
 
 echo "  mapper/formats/gcpBind."
 cd mapper/formats/gcpBind
 go build ./...
+doTest
 cd ../../..
 
 echo "  mapTool."
-go build -o mapTool cmd/mapTool/main.go
+cd cmd/mapTool
+go build -o ../../mapTool main.go
+doTest
+cd ../../
