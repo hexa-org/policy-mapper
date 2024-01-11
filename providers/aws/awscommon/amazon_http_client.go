949a6fa1ec33576e354705d3397ee90afb801085
Phil Hunt <phil.hunt@independentid.com>
2024-01-11 13:35:31 -0800

    Updated policyprovider case, updated dependencies
    Signed-off-by: Phil Hunt <phil.hunt@independentid.com>

--- providers/aws/awscommon/amazon_http_client.go
+++ providers/aws/awscommon/amazon_http_client.go
@@ -1,51 +1,52 @@
 package awscommon
 
 import (
-	"bytes"
-	"context"
-	"encoding/json"
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	awscredentials "github.com/aws/aws-sdk-go-v2/credentials"
-	"net/http"
+    "bytes"
+    "context"
+    "encoding/json"
+    "net/http"
+
+    "github.com/aws/aws-sdk-go-v2/aws"
+    "github.com/aws/aws-sdk-go-v2/config"
+    awscredentials "github.com/aws/aws-sdk-go-v2/credentials"
 )
 
 type AWSHttpClient interface {
-	Do(req *http.Request) (*http.Response, error)
+    Do(req *http.Request) (*http.Response, error)
 }
 
 type AWSClientOptions struct {
-	HTTPClient   AWSHttpClient
-	DisableRetry bool
+    HTTPClient   AWSHttpClient
+    DisableRetry bool
 }
 
 func GetAwsClientConfig(key []byte, opt AWSClientOptions) (aws.Config, error) {
-	var awsCredentials credentialsInfo
-	err := json.NewDecoder(bytes.NewReader(key)).Decode(&awsCredentials)
-	if err != nil {
-		return aws.Config{}, err
-	}
+    var awsCredentials credentialsInfo
+    err := json.NewDecoder(bytes.NewReader(key)).Decode(&awsCredentials)
+    if err != nil {
+        return aws.Config{}, err
+    }
 
-	awsOptions := []func(options *config.LoadOptions) error{
-		config.WithCredentialsProvider(awscredentials.StaticCredentialsProvider{
-			Value: aws.Credentials{AccessKeyID: awsCredentials.AccessKeyID, SecretAccessKey: awsCredentials.SecretAccessKey},
-		}),
-		config.WithRegion(awsCredentials.Region),
-	}
+    awsOptions := []func(options *config.LoadOptions) error{
+        config.WithCredentialsProvider(awscredentials.StaticCredentialsProvider{
+            Value: aws.Credentials{AccessKeyID: awsCredentials.AccessKeyID, SecretAccessKey: awsCredentials.SecretAccessKey},
+        }),
+        config.WithRegion(awsCredentials.Region),
+    }
 
-	if opt.HTTPClient != nil {
-		awsOptions = append(awsOptions, config.WithHTTPClient(opt.HTTPClient))
-	}
+    if opt.HTTPClient != nil {
+        awsOptions = append(awsOptions, config.WithHTTPClient(opt.HTTPClient))
+    }
 
-	if opt.DisableRetry {
-		awsOptions = append(awsOptions, config.WithRetryer(func() aws.Retryer { return aws.NopRetryer{} }))
-	}
+    if opt.DisableRetry {
+        awsOptions = append(awsOptions, config.WithRetryer(func() aws.Retryer { return aws.NopRetryer{} }))
+    }
 
-	return config.LoadDefaultConfig(context.Background(), awsOptions...)
+    return config.LoadDefaultConfig(context.Background(), awsOptions...)
 }
 
 type credentialsInfo struct {
-	AccessKeyID     string `json:"accessKeyID"`
-	SecretAccessKey string `json:"secretAccessKey"`
-	Region          string `json:"region"`
+    AccessKeyID     string `json:"accessKeyID"`
+    SecretAccessKey string `json:"secretAccessKey"`
+    Region          string `json:"region"`
 }
