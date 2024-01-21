ed0f3d065a4a81d86878073bc9b3f53a303a4b54
Phil Hunt <phil.hunt@independentid.com>
2024-01-20 16:09:55 -0800

    OPA Provider refactored to run in Policy-Mappper Signed-off-by: Phil Hunt <phil.hunt@independentid.com>
    Signed-off-by: Phil Hunt <phil.hunt@independentid.com>

---
+++ providers/openpolicyagent/http_bundle_client_test.go
@@ -0,0 +1,129 @@
+package openpolicyagent_test
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+
+	"math/rand"
+	"os"
+	"path/filepath"
+	"runtime"
+	"testing"
+	"time"
+
+	"github.com/hexa-org/policy-mapper/pkg/testsupport"
+	"github.com/hexa-org/policy-mapper/providers/openpolicyagent"
+	"github.com/hexa-org/policy-mapper/providers/openpolicyagent/compressionsupport"
+	"github.com/stretchr/testify/assert"
+)
+
+const expectedBundleData = `{
+  "policies": [
+    {
+      "meta": {"version": "0.5"},
+      "actions": [{"action_uri": "http:GET:/"}],
+      "subject": {
+        "members": [
+          "allusers", "allauthenticated"
+        ]
+      },
+      "object": {
+        "resource_id": "aResourceId"
+      }
+    },
+    {
+      "meta": {"version": "0.5"},
+      "actions": [{"action_uri": "http:GET:/sales"}, {"action_uri": "http:GET:/marketing"}],
+      "subject": {
+        "members": [
+          "allauthenticated",
+          "sales@hexaindustries.io",
+          "marketing@hexaindustries.io"
+        ]
+      },
+      "object": {
+        "resource_id": "aResourceId"
+      }
+    },
+    {
+      "meta": {"version": "0.5"},
+      "actions": [{"action_uri": "http:GET:/accounting"}, {"action_uri": "http:POST:/accounting"}],
+      "subject": {
+        "members": [
+          "accounting@hexaindustries.io"
+        ]
+      },
+      "object": {
+        "resource_id": "aResourceId"
+      }
+    },
+    {
+      "meta": {"version": "0.5"},
+      "actions": [{"action_uri": "http:GET:/humanresources"}],
+      "subject": {
+        "members": [
+          "humanresources@hexaindustries.io"
+        ]
+      },
+      "object": {
+        "resource_id": "aResourceId"
+      }
+    }
+  ]
+}`
+
+func TestHTTPBundleClient_GetExpressionFromBundle(t *testing.T) {
+	_, file, _, _ := runtime.Caller(0)
+	join := filepath.Join(file, "../resources/bundles")
+	tar, _ := compressionsupport.TarFromPath(join)
+	var buffer bytes.Buffer
+	_ = compressionsupport.Gzip(&buffer, tar)
+	random := rand.New(rand.NewSource(time.Now().UnixNano()))
+	path := filepath.Join(t.TempDir(), fmt.Sprintf("test-bundles/.bundle-%d", random.Uint64()))
+
+	m := testsupport.NewMockHTTPClient()
+	m.ResponseBody["someURL"] = buffer.Bytes()
+	client := openpolicyagent.HTTPBundleClient{BundleServerURL: "someURL", HttpClient: m}
+
+	data, err := client.GetDataFromBundle(path)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expectedBundleData, string(data))
+}
+
+func TestHTTPBundleClient_GetExpressionFromBundle_withBadTar(t *testing.T) {
+	_, file, _, _ := runtime.Caller(0)
+	join := filepath.Join(file, "../resources/bundles")
+	tar, _ := compressionsupport.TarFromPath(join)
+	var buffer bytes.Buffer
+	_ = compressionsupport.Gzip(&buffer, tar)
+
+	m := testsupport.NewMockHTTPClient()
+	m.ResponseBody["someURL"] = buffer.Bytes()
+	client := openpolicyagent.HTTPBundleClient{BundleServerURL: "someURL", HttpClient: m}
+
+	_, err := client.GetDataFromBundle("/badPath")
+
+	assert.Contains(t, err.Error(), "unable to untar to path")
+}
+
+func TestHTTPBundleClient_GetExpressionFromBundle_withBadRequest(t *testing.T) {
+	m := testsupport.NewMockHTTPClient()
+	m.Err = errors.New("oops")
+	client := openpolicyagent.HTTPBundleClient{BundleServerURL: "someURL", HttpClient: m}
+
+	_, err := client.GetDataFromBundle(os.TempDir())
+
+	assert.EqualError(t, err, "oops")
+}
+
+func TestHTTPBundleClient_GetExpressionFromBundle_withBadGzip(t *testing.T) {
+	m := testsupport.NewMockHTTPClient()
+	m.ResponseBody["someURL"] = []byte("bad gzip")
+	client := openpolicyagent.HTTPBundleClient{BundleServerURL: "someURL", HttpClient: m}
+
+	_, err := client.GetDataFromBundle("")
+
+	assert.Contains(t, err.Error(), "unable to ungzip")
+}
